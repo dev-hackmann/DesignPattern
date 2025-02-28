1d8cfa3d69afecb39435dff68c5777ea8819bb0e
Hackmann <hackmann657@gmail.com>
2025-02-28 08:24:22 -0300

    Factory Method

---
+++ Criacional/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NotificationAppCreational
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotification notification = NotificationFactory.CreateNotification("sms", "Olá, mundo!");
+            notification.Send();
+        }
+    }
+    
+    public static class NotificationFactory
+    {
+        public static INotification CreateNotification(string type, string message)
+        {
+            return type.ToLower() switch
+            {
+                "email" => new EmailNotification(message),
+                "sms" => new SMSNotification(message),
+                _ => throw new ArgumentException("Tipo de notificação inválido")
+            };
+        }
+    }
+
+    public interface INotification
+    {
+        void Send();
+    }
+
+    public class EmailNotification : INotification
+    {
+        private readonly string _message;
+        public EmailNotification(string message) => _message = message;
+        public void Send() => Console.WriteLine($"Email: {_message}");
+    }
+
+    public class SMSNotification : INotification
+    {
+        private readonly string _message;
+        public SMSNotification(string message) => _message = message;
+        public void Send() => Console.WriteLine($"SMS: {_message}");
+    }
+}
